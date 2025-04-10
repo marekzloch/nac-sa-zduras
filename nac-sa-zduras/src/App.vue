f26c482df410a2c283294595109a4d0e6de422ab
Marek Zloch <56075692+MZ-2406@users.noreply.github.com>
2025-04-10 22:37:51 +0200

    test commit 2

--- nac-sa-zduras/src/App.vue
+++ nac-sa-zduras/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vdasdadsue nikdy sa nezavdacim vseckym" />
+  <HelloWorld msg="Vite + Vdasdadsdsaue nikdy sa nezavdacim vseckym" />
 </template>
 
 <style scoped>
