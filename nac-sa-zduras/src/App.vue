04fea7e73117d2ccda1aaa44573069e21636ad36
Marek Zloch <56075692+MZ-2406@users.noreply.github.com>
2025-04-11 11:52:44 +0200

    test 1

--- nac-sa-zduras/src/App.vue
+++ nac-sa-zduras/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vdasdadsdsaue nikdy  test sa nezavdacim vseckym" />
+  <HelloWorld msg="Vite + Vdasdadsdsaue nikdy  test sa nezavdacim vseckym test 1 " />
 </template>
 
 <style scoped>
